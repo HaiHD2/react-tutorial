85e571dbd8d013546832b54a7f6e3881a87f7e41
haihd2 <sealoe1996@gmail.com>
2024-02-19 23:46:58 +0700

    component create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Product from './component/Product';
 
 function App() {
     return (
         <div className="App">
             <h1>Hello World</h1>
+            <Product></Product>
         </div>
     );
 }
---
+++ src/component/Product.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Product() {
+    return (
+        <React.Fragment>
+            <h1>Product</h1>
+            <button>create product</button>
+        </React.Fragment>
+        // OR
+        // <>
+        //     <h1>Product</h1>
+        //     <button>create product</button>
+        // </>
+    );
+}
+
+export default Product;
