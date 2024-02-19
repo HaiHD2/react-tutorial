00779dce377e6d02bed5c14f6081fe154ed6e177
haihd2 <sealoe1996@gmail.com>
2024-02-20 00:16:30 +0700

    create state first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let stateValue; // Vị trí khai báo: global
+
+function useState(intialState) {
+    if (stateValue === undefined) {
+        stateValue = intialState;
+    }
+    function setValue(value) {
+        stateValue = value;
+        ReactDOM.createRoot(document.getElementById("root")).render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        );
+    }
+
+    return [stateValue, setValue];
+}
 
 function App() {
+    const [counter, setCounter] = useState(1);
+
+    const handleIncrement = function () {
+        setCounter(counter + 1);
+    };
+
+    const handleDecrement = function () {
+        setCounter(counter - 1);
+    };
+
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1>Counter {counter}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
         </div>
     );
 }
 
 export default App;
+
+// Tạo state để quản lí counter
+// Khi state thay đổi, thì giao diện thay đổi
