5e73eb36104a3fabbd5c2da1cfd13b794107e456
haihd2 <sealoe1996@gmail.com>
2024-02-20 22:03:45 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-import Counter from "./component/Counter"
 
 
 function App() {
-    const urlImage = "https://cdn.urldecoder.org/assets/images/url-fb-en.png";
-
     return (
         <div className="App">
-            <img className="image_feature" src={urlImage}/>
             <h1>Hello World</h1>
-            <Counter/>
         </div>
     );
 }
