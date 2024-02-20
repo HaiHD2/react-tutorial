ce991e3ae603ad8af00b1058e3ed0596d77cdcf9
haihd2 <sealoe1996@gmail.com>
2024-02-20 23:25:47 +0700

    arrow function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 
 function App() {
+
+    const [tags, setTags] = useState(['tag1', 'tag2','tag3']);
+
     return (
         <div className="App">
+            <ul>
+                {
+                    tags.map(
+                        (tag) => {
+                            return (
+                                <li>{tag}</li>
+                            )
+                        }
+                    )
+                }
+            </ul>
             <h1>Hello World</h1>
         </div>
     );
