0be03f249f8381e584a7b4f09473030729bd68c2
haihd2 <sealoe1996@gmail.com>
2024-02-20 22:21:22 +0700

    class dynamic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 
+
 function App() {
+    const [counter, setCounter] = useState(0);
+
+    const formatClass = function() {
+        let classCommon = "badge p-3 ";
+
+        if (counter == 0) {
+            classCommon += "badge-warning";
+        } else {
+            classCommon += "badge-primary";
+        }
+
+        return classCommon;
+    }
     return (
         <div className="App">
             <h1>Hello World</h1>
+            <span class={formatClass()}>Primary</span>
         </div>
     );
 }
