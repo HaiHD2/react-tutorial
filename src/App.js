168213c2c17d3164a360aed01203ddd607c360f5
haihd2 <sealoe1996@gmail.com>
2024-02-20 22:35:46 +0700

    rendering lists

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
-
+import {useState} from "react";
 
 function App() {
+    const [tags, setTags] = useState(['tag1','tag2','tag3']);
+
+    const renderTags = function() {
+        const listTags = tags.map(function(tag) {
+            return (
+                <li>{tag}</li>
+            )
+        });
+        return listTags;
+    }
     return (
         <div className="App">
+            <ul>
+                {
+                    tags.map(function(tag) {
+                        return (
+                            <li key={tag}>{tag}</li>
+                        )
+                    })
+                }
+            </ul>
             <h1>Hello World</h1>
         </div>
     );
