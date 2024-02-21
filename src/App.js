713cd52d429d8bbbee1d45181833d25e0d51ed81
haihd2 <sealoe1996@gmail.com>
2024-02-21 09:50:49 +0700

    event react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 
 
 function App() {
+
+    const [counter, setCounter] = useState(1);
+
+    const handleIncrement = () => {
+        setCounter(counter + 1);
+    }
+
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1>Counter {counter}</h1>
+            <button onClick={handleIncrement}>
+                Increment
+            </button>
         </div>
     );
 }
 
 export default App;
+
+
+// Lần render đầu tiên
+    // Điều kiện để trở thành closure
+    // 1. Phải là hàm được bao đóng trong function
+    // 2. Phải sử dụng biến của function bên ngoài
+
+// Lần thứ n render
+    // Điều kiện để trở thành closure
+    // 1. Phải là hàm được bao đóng trong function
+    // 2. Phải sử dụng biến của function bên ngoài
