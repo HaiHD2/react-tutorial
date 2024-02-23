b365542a9df90ea7c7f50b5fda048d3c4a1727a9
haihd2 <sealoe1996@gmail.com>
2024-02-23 21:53:24 +0700

    react route dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import {Routes, Route, Link} from "react-router-dom"
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./pages/Home";
+import NewsPage from "./pages/News";
+import ContactPage from "./pages/Contact"; 
 
-
-function App() {
+function App(){
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <nav>
+                <ul className="navbar-nav">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/news" element={<NewsPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
         </div>
-    );
+    )
 }
 
 export default App;
