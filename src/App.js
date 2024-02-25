5e9465c6f52674c7f9f236433aba6de493a6e865
haihd2 <sealoe1996@gmail.com>
2024-02-25 22:24:54 +0700

    useEffect với mảng rỗng

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import { useEffect } from "react";
 import "./App.css";
-
+import { useState } from "react";
 
 function App() {
+
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        console.log("useEffect running")
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+            .then((response) => response.json())
+            .then((json) => {
+                setPost(json);
+            });
+    }, []);
+
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <div>
+                <h1>{post.id}</h1>
+                <h1>{post.title}</h1>
+            </div>
         </div>
     );
 }
 
 export default App;
+
+//1. Tại sao lại bị hiện tượng call api liên tục
+
+//Lần đầu render App version 001
+// useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/todos/1")
+//             .then((response) => response.json())
+//             .then((json) => {
+//                 setPost(json); // {} -> {data với id = 1} -> component re-render
+//             });
+//     });
+
+//Lần thứ 2 re-render App version 002
+// useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/todos/1")
+//             .then((response) => response.json())
+//             .then((json) => {
+//                 setPost(json); // {} -> {data với id = 1} -> {data với id = 1} -> state thay đổi -> App re-render
+//             });
+//     });
+
+// 
